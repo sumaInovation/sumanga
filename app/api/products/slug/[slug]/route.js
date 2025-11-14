fd1d583380eed32d08e204584227b92fdaea42be
sumanga wimaladasa <sumanga0000@gmail.com>
2025-11-14 23:26:12 +0530

    Update route.js

--- app/api/products/slug/[slug]/route.js
+++ app/api/products/slug/[slug]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import Product from '@/models/Product';
-import dbConnect from '@/lib/mongodb';
+import { connectDB as dbConnect } from '@/lib/mongodb';
 
 export async function GET(request, { params }) {
   try {
