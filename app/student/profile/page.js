9f140acedebc29fd87c0d253baded42f38408269
Sumanga <sumanga0000@gmail.com>
2025-11-17 15:50:16 +0530

    add

---
+++ app/student/profile/page.js
@@ -0,0 +1,13 @@
+// app/student/profile/page.js
+'use client';
+
+export default function AdminPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Admin Page</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
+
+export const dynamic = 'force-dynamic';
